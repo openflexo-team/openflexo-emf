9c82cde932b375d76b4e67d5e98a7515a0f1b4f7
Sylvain GUERIN <sylvain.guerin@openflexo.org>
2021-06-25 11:14:19 +0200

    MEDIUM / Fixed object id management

--- emf-ta/src/main/java/org/openflexo/technologyadapter/emf/rm/EMFMetaModelResourceImpl.java
+++ emf-ta/src/main/java/org/openflexo/technologyadapter/emf/rm/EMFMetaModelResourceImpl.java
@@ -50,14 +50,20 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.logging.Logger;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.openflexo.foundation.FlexoException;
+import org.openflexo.foundation.FlexoObject;
+import org.openflexo.foundation.InnerResourceData;
 import org.openflexo.foundation.resource.FileIODelegate;
 import org.openflexo.foundation.resource.FlexoIODelegate;
 import org.openflexo.foundation.resource.FlexoResourceImpl;
 import org.openflexo.foundation.resource.InJarIODelegate;
+import org.openflexo.foundation.resource.ResourceData;
 import org.openflexo.foundation.resource.ResourceLoadingCancelledException;
+import org.openflexo.technologyadapter.emf.metamodel.AEMFMetaModelObjectImpl;
 import org.openflexo.technologyadapter.emf.metamodel.EMFMetaModel;
 import org.openflexo.technologyadapter.emf.metamodel.io.EMFMetaModelConverter;
 
@@ -244,4 +250,60 @@ public abstract class EMFMetaModelResourceImpl extends FlexoResourceImpl<EMFMeta
 			return file;
 		}
 	}
+
+	/**
+	 * Generic method used to retrieve in this resource an object with supplied objectIdentifier, userIdentifier, and type identifier<br>
+	 * 
+	 * Note that for certain resources, some parameters might not be used (for example userIdentifier or typeIdentifier)
+	 * 
+	 * @param objectIdentifier
+	 * @param userIdentifier
+	 * @param typeIdentifier
+	 * @return
+	 */
+	@Override
+	public FlexoObject findObject(String objectIdentifier, String userIdentifier, String typeIdentifier) {
+		System.out.println("Dans EMFMetaModelResource, on me demande de trouver l'objet objectIdentifier=" + objectIdentifier
+				+ " userIdentifier=" + userIdentifier + " typeIdentifier=" + typeIdentifier);
+		// return getFlexoObject(Long.parseLong(objectIdentifier), userIdentifier);
+		return null;
+	}
+
+	/**
+	 * Used to compute identifier of an object asserting this object is the {@link ResourceData} itself, or a {@link InnerResourceData}
+	 * object stored inside this resource
+	 * 
+	 * @param object
+	 * @return a String identifying supplied object (semantics is composite key using userIdentifier and typeIdentifier)
+	 */
+	@Override
+	public String getObjectIdentifier(Object object) {
+
+		if (object instanceof AEMFMetaModelObjectImpl) {
+			EObject eObject = ((AEMFMetaModelObjectImpl) object).getObject();
+			return EcoreUtil.getID(eObject);
+			/*if (eObject instanceof ENamedElement) {
+				return ((ENamedElement) eObject).getName();
+			}
+			else {
+				logger.warning("Could not find id for " + object);
+				return null;
+			}*/
+		}
+		logger.warning("Unexpected object " + object);
+		return null;
+	}
+
+	/**
+	 * Used to compute user identifier of an object asserting this object is the {@link ResourceData} itself, or a {@link InnerResourceData}
+	 * object stored inside this resource
+	 * 
+	 * @param object
+	 * @return a String identifying author (user) of supplied object
+	 */
+	@Override
+	public String getUserIdentifier(Object object) {
+		return "FLX";
+	}
+
 }
