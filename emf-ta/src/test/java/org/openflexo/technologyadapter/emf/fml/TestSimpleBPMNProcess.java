5bd68d3f8cdcdbb697c2c423bbf7e97a02d3ebf5
Sylvain Guérin <sylvain.guerin@ensta-bretagne.org>
2023-05-19 18:42:41 +0200

    MEDIUM / Unit test

--- emf-ta/src/test/java/org/openflexo/technologyadapter/emf/fml/TestSimpleBPMNProcess.java
+++ emf-ta/src/test/java/org/openflexo/technologyadapter/emf/fml/TestSimpleBPMNProcess.java
@@ -38,15 +38,20 @@
 
 package org.openflexo.technologyadapter.emf.fml;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.openflexo.connie.exception.InvalidBindingException;
+import org.openflexo.connie.exception.NullReferenceException;
+import org.openflexo.connie.exception.TypeMismatchException;
 import org.openflexo.foundation.FlexoEditor;
 import org.openflexo.foundation.FlexoException;
 import org.openflexo.foundation.FlexoProject;
@@ -57,12 +62,15 @@ import org.openflexo.foundation.fml.rm.CompilationUnitResource;
 import org.openflexo.foundation.fml.rt.FMLRTVirtualModelInstance;
 import org.openflexo.foundation.fml.rt.action.CreateBasicVirtualModelInstance;
 import org.openflexo.foundation.fml.rt.rm.FMLRTVirtualModelInstanceResource;
+import org.openflexo.foundation.ontology.IFlexoOntologyClass;
+import org.openflexo.foundation.ontology.IFlexoOntologyIndividual;
 import org.openflexo.foundation.resource.FlexoResource;
 import org.openflexo.foundation.resource.FlexoResourceCenter;
 import org.openflexo.foundation.resource.ResourceLoadingCancelledException;
 import org.openflexo.foundation.test.OpenflexoProjectAtRunTimeTestCase;
 import org.openflexo.rm.ResourceLocator;
 import org.openflexo.technologyadapter.emf.EMFTechnologyAdapter;
+import org.openflexo.technologyadapter.emf.model.EMFObjectIndividual;
 import org.openflexo.technologyadapter.emf.rm.EMFModelResource;
 import org.openflexo.test.OrderedRunner;
 import org.openflexo.test.TestOrder;
@@ -83,11 +91,10 @@ public class TestSimpleBPMNProcess extends OpenflexoProjectAtRunTimeTestCase {
 	private static VirtualModel rootVM;
 
 	private static FMLRTVirtualModelInstance vmi;
-	private static EMFModelResource animalOntologyResource;
+	private static EMFModelResource basicExampleResource;
 
 	@Test
 	@TestOrder(1)
-	@Category(UITest.class)
 	public void testLoadViewPoint() throws FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
 		instanciateTestServiceManager(EMFTechnologyAdapter.class);
 
@@ -95,7 +102,7 @@ public class TestSimpleBPMNProcess extends OpenflexoProjectAtRunTimeTestCase {
 		serviceManager.activateTechnologyAdapter(emfTA, true);
 
 		FlexoResourceCenter<?> resourceCenter = serviceManager.getResourceCenterService()
-				.getFlexoResourceCenter("http://openflexo.org/owl-test");
+				.getFlexoResourceCenter("http://openflexo.org/emf-test");
 
 		System.out.println("emfTA=" + emfTA);
 		System.out.println("resourceCenter=" + resourceCenter);
@@ -105,12 +112,16 @@ public class TestSimpleBPMNProcess extends OpenflexoProjectAtRunTimeTestCase {
 		}
 
 		CompilationUnitResource cur = (CompilationUnitResource) serviceManager.getResourceManager()
-				.getResource("http://www.openflexo.org/test/owl/Animals.fml");
+				.getResource("http://www.openflexo.org/test/emf/SimpleBPMNProcess.fml");
 		System.out.println("Hop: " + cur);
+
+		for (FlexoResource<?> dep : cur.getDependencies()) {
+			System.out.println(" >>> " + dep);
+		}
 
 		VirtualModelLibrary vpLib = serviceManager.getVirtualModelLibrary();
 		assertNotNull(vpLib);
-		rootVM = vpLib.getVirtualModel("http://www.openflexo.org/test/owl/Animals.fml");
+		rootVM = vpLib.getVirtualModel("http://www.openflexo.org/test/emf/SimpleBPMNProcess.fml");
 
 		assertNotNull(rootVM);
 
@@ -118,16 +129,24 @@ public class TestSimpleBPMNProcess extends OpenflexoProjectAtRunTimeTestCase {
 
 		assertCompilationUnitIsValid(rootVM.getCompilationUnit());
 
-		animalOntologyResource = (EMFModelResource) serviceManager.getResourceManager().getResource("http://openflexo.org/test/animals");
-		assertNotNull(animalOntologyResource);
-
 		System.out.println(rootVM.getCompilationUnit().getNormalizedFML());
+
+		basicExampleResource = (EMFModelResource) serviceManager.getResourceManager()
+				.getResource("http://openflexo.org/emf-test/TestResourceCenter/EMF/Model/bpmn/BasicExample.bpmn");
+		assertNotNull(basicExampleResource);
+
+		for (IFlexoOntologyClass<EMFTechnologyAdapter> c : basicExampleResource.getModel().getMetaModel().getAccessibleClasses()) {
+			System.out.println(" >>>> " + c.getName() + " uri=" + c.getURI());
+		}
+
+		for (IFlexoOntologyIndividual<EMFTechnologyAdapter> ind : basicExampleResource.getModel().getIndividuals()) {
+			System.out.println(" > " + ind + " of " + ind.getTypes());
+		}
 
 	}
 
 	@Test
 	@TestOrder(2)
-	@Category(UITest.class)
 	public void testCreateProject() {
 		editor = createStandaloneProject("TestProject");
 		project = (FlexoProject<File>) editor.getProject();
@@ -137,20 +156,19 @@ public class TestSimpleBPMNProcess extends OpenflexoProjectAtRunTimeTestCase {
 
 	@Test
 	@TestOrder(3)
-	@Category(UITest.class)
 	public void testCreateInstance() {
 
 		System.err.println("testCreateInstance()");
 		CreateBasicVirtualModelInstance action = CreateBasicVirtualModelInstance.actionType
 				.makeNewAction(project.getVirtualModelInstanceRepository().getRootFolder(), null, editor);
-		action.setNewVirtualModelInstanceName("AnimalsInstance");
-		action.setNewVirtualModelInstanceTitle("Test animals");
+		action.setNewVirtualModelInstanceName("TestInstance");
+		action.setNewVirtualModelInstanceTitle("Test simple BPMN process");
 		action.setVirtualModel(rootVM);
 
 		CreationScheme creationScheme = rootVM.getCreationSchemes().get(0);
 		action.setCreationScheme(creationScheme);
 
-		action.setParameterValue(creationScheme.getParameters().get(0), animalOntologyResource);
+		action.setParameterValue(creationScheme.getParameters().get(0), basicExampleResource);
 		action.doAction();
 		assertTrue(action.hasActionExecutionSucceeded());
 		vmi = action.getNewVirtualModelInstance();
@@ -163,96 +181,97 @@ public class TestSimpleBPMNProcess extends OpenflexoProjectAtRunTimeTestCase {
 
 	}
 
-	/*	@Test
-		@TestOrder(4)
-		@Category(UITest.class)
-		public void testListAllOWLClasses() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
-				InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
-			System.err.println("testListAllOWLClasses()");
-			List<OWLClass> allClasses = vmi.execute("this.listAllOWLClasses()");
-			assertEquals(30, allClasses.size());
-		}
+	@Test
+	@TestOrder(4)
+	@Category(UITest.class)
+	public void testListAllEMFObjectIndividuals() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
+			InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
+		System.err.println("testListAllEMFObjectIndividuals()");
+		List<EMFObjectIndividual> allObjects = vmi.execute("this.listAllEMFIndividuals()");
+		assertEquals(34, allObjects.size());
+	}
+
+	/*
+	@Test
+	@TestOrder(5)
+	@Category(UITest.class)
+	public void testListDeclaredOWLClasses() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
+			InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
+		System.err.println("listDeclaredOWLClasses()");
+		List<OWLClass> allClasses = vmi.execute("this.listDeclaredOWLClasses()");
+		assertEquals(4, allClasses.size());
+	}
 	
-		@Test
-		@TestOrder(5)
-		@Category(UITest.class)
-		public void testListDeclaredOWLClasses() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
-				InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
-			System.err.println("listDeclaredOWLClasses()");
-			List<OWLClass> allClasses = vmi.execute("this.listDeclaredOWLClasses()");
-			assertEquals(4, allClasses.size());
-		}
+	@Test
+	@TestOrder(6)
+	@Category(UITest.class)
+	public void testListDeclaredOWLClasses2() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
+			InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
+		System.err.println("listDeclaredOWLClasses2()");
+		List<OWLClass> allClasses = vmi.execute("this.listDeclaredOWLClasses2()");
+		assertEquals(4, allClasses.size());
+	}
 	
-		@Test
-		@TestOrder(6)
-		@Category(UITest.class)
-		public void testListDeclaredOWLClasses2() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
-				InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
-			System.err.println("listDeclaredOWLClasses2()");
-			List<OWLClass> allClasses = vmi.execute("this.listDeclaredOWLClasses2()");
-			assertEquals(4, allClasses.size());
-		}
+	@Test
+	@TestOrder(7)
+	@Category(UITest.class)
+	public void testSelectUniqueOWLClass() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
+			InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
+		System.err.println("selectUniqueOWLClass()");
+		OWLClass theCat = vmi.execute("this.selectUniqueOWLClass()");
+		System.err.println("theCat=" + theCat);
+		assertNotNull(theCat);
+	}
 	
-		@Test
-		@TestOrder(7)
-		@Category(UITest.class)
-		public void testSelectUniqueOWLClass() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
-				InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
-			System.err.println("selectUniqueOWLClass()");
-			OWLClass theCat = vmi.execute("this.selectUniqueOWLClass()");
-			System.err.println("theCat=" + theCat);
-			assertNotNull(theCat);
-		}
+	@Test
+	@TestOrder(8)
+	@Category(UITest.class)
+	public void testListAllOWLIndividuals() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
+			InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
+		System.err.println("listAllOWLIndividuals()");
+		List<OWLIndividual> allIndividuals = vmi.execute("this.listAllOWLIndividuals()");
+		assertEquals(3, allIndividuals.size());
+	}
 	
-		@Test
-		@TestOrder(8)
-		@Category(UITest.class)
-		public void testListAllOWLIndividuals() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
-				InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
-			System.err.println("listAllOWLIndividuals()");
-			List<OWLIndividual> allIndividuals = vmi.execute("this.listAllOWLIndividuals()");
-			assertEquals(3, allIndividuals.size());
-		}
+	@Test
+	@TestOrder(9)
+	@Category(UITest.class)
+	public void testListAllOWLIndividuals2() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
+			InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
+		System.err.println("listAllOWLIndividuals2()");
+		List<OWLIndividual> allIndividuals = vmi.execute("this.listAllOWLIndividuals2()");
+		assertEquals(3, allIndividuals.size());
+	}
 	
-		@Test
-		@TestOrder(9)
-		@Category(UITest.class)
-		public void testListAllOWLIndividuals2() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
-				InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
-			System.err.println("listAllOWLIndividuals2()");
-			List<OWLIndividual> allIndividuals = vmi.execute("this.listAllOWLIndividuals2()");
-			assertEquals(3, allIndividuals.size());
-		}
+	@Test
+	@TestOrder(10)
+	@Category(UITest.class)
+	public void testSelectUniqueOWLIndividual() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
+			InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
+		System.err.println("selectUniqueOWLIndividual()");
+		OWLIndividual jerry = vmi.execute("this.selectUniqueOWLIndividual()");
+		System.err.println("jerry=" + jerry);
+		assertNotNull(jerry);
+	}
 	
-		@Test
-		@TestOrder(10)
-		@Category(UITest.class)
-		public void testSelectUniqueOWLIndividual() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
-				InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
-			System.err.println("selectUniqueOWLIndividual()");
-			OWLIndividual jerry = vmi.execute("this.selectUniqueOWLIndividual()");
-			System.err.println("jerry=" + jerry);
-			assertNotNull(jerry);
-		}
+	@Test
+	@TestOrder(11)
+	@Category(UITest.class)
+	public void testListAllCats() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
+			InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
+		System.err.println("listAllCats()");
+		List<OWLIndividual> allCats = vmi.execute("this.listAllCats()");
+		assertEquals(2, allCats.size());
+	}
 	
-		@Test
-		@TestOrder(11)
-		@Category(UITest.class)
-		public void testListAllCats() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
-				InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
-			System.err.println("listAllCats()");
-			List<OWLIndividual> allCats = vmi.execute("this.listAllCats()");
-			assertEquals(2, allCats.size());
-		}
-	
-		@Test
-		@TestOrder(12)
-		@Category(UITest.class)
-		public void testListAllCats2() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
-				InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
-			System.err.println("listAllCats2()");
-			List<OWLIndividual> allCats = vmi.execute("this.listAllCats2()");
-			assertEquals(2, allCats.size());
-		}
+	@Test
+	@TestOrder(12)
+	@Category(UITest.class)
+	public void testListAllCats2() throws TypeMismatchException, NullReferenceException, ReflectiveOperationException,
+			InvalidBindingException, FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
+		System.err.println("listAllCats2()");
+		List<OWLIndividual> allCats = vmi.execute("this.listAllCats2()");
+		assertEquals(2, allCats.size());
+	}
 	*/
 }
